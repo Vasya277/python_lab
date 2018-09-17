e26b8dd73f8f31b06c7797476d457855845efd99
Vasya277 <31722005+Vasya277@users.noreply.github.com>
2018-09-17 12:10:07 +0300

    Create lab2_2.py

---
+++ lab2_2.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+
+import sys
+
+surname = input("Enter your surname: ")
+name = input("Enter your name: ")
+father_name = input("Enter your father's name: ")
+
+full_name = surname  + " " + name + " " + father_name 
+
+print ("Hello, " + full_name + "!")
